a7fe44c4792a8e4bcb7dc7597516ce892f5f2431
Nelio Alves <acenelio@gmail.com>
2018-11-02 16:51:32 -0300

    Initial implementation

---
+++ src/application/Program.java
@@ -0,0 +1,37 @@
+package application;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import db.DB;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Connection conn = null;
+		Statement st = null;
+		ResultSet rs = null;
+		try {
+			conn = DB.getConnection();
+	
+			st = conn.createStatement();
+			
+			rs = st.executeQuery("select * from department");
+			
+			while (rs.next()) {
+				System.out.println(rs.getInt("Id") + ", " + rs.getString("Name"));
+			}
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+		finally {
+			DB.closeResultSet(rs);
+			DB.closeStatement(st);
+			DB.closeConnection();
+		}
+	}
+}
